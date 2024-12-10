99a513fcca43ccaaa4aecd9d6add6e86a79b5166
Joshua Scott <joshua.ds.s019@gmail.com>
2024-12-10 12:05:59 -0300

    feat: blog pages with styles

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const posts = [];
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -15,7 +17,17 @@ app.use(express.static(path.join(process.cwd(), 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.render('index', { posts: [] });
+  res.render('index', { posts });
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/new', (req, res) => {
+  const { title, content } = req.body;
+  posts.push({ title, content });
+  res.redirect('/');
 });
 
 // Start Server
